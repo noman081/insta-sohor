41fb0e2137468d91d795bcc46de871371d34fe34
noman081 <abdullahalnoman642@gmail.com>
2022-03-10 21:51:29 +0600

    After fixing liked picture bug

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  likedPostsId.plus(id);
+  likedPostsId.push(id);
   showPosts(posts);
 };
 
@@ -144,6 +144,10 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+  if (likedPosts.length === 0) {
+    return;
+  }
+  document.getElementById("liked").textContent = '';
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
